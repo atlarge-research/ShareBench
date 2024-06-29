64b11d2a3c57500bc3141e9595b477a439c91066
lkm.schulz <lkm.schulz@gmail.com>
2024-06-29 21:57:51 +0200

    feat: image python script

---
+++ scripts/push_image.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import argparse
+import yaml
+import jinja2 as jin
+
+CONFIG = 'config.yaml'
+KEY = 'docker'
+
+def build_program():
+    sdkman_init_script = os.path.expanduser("~/.sdkman/bin/sdkman-init.sh")
+    command = f'source {sdkman_init_script} && sbt package'
+    subprocess.run(command, shell=True, executable='/bin/bash', check=True)
+
+def docker_build(uri_image):
+    subprocess.run(['docker', 'build', '--platform', 'linux/amd64', '-t', uri_image, '-f', 'docker/Dockerfile', '.'], check=True)
+
+def docker_push(uri_image):
+    subprocess.run(['docker', 'login'], check=True)
+    subprocess.run(['docker', 'push', uri_image], check=True)
+
+def push_image(config, build = True):
+
+    username = config[KEY]['username']
+    image = config['general']['name']
+    version = config[KEY]['version']
+    uri = f"{username}/{image}:{version}"
+
+    if build:
+        print("Building the project...")
+        build_program()
+
+    docker_build(uri)
+    docker_push(uri)
+
+if __name__ == "__main__":
+    with open(CONFIG, 'r') as file:
+        config = yaml.safe_load(file)
+
+    parser = argparse.ArgumentParser(description='Build and push the docker image.')
+    parser.add_argument('-b', '--build', action='store_true', help='Build the program from source.')
+    args = parser.parse_args()
+
+    push_image(config, args.build)
